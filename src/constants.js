836b0c9aa964b7e39c787d22af754dfcabf235a8
MananSoni <manansoni.soni77@gmail.com>
2023-10-24 22:46:47 +0530

    Add contants for skills and projects

---
+++ src/constants.js
@@ -0,0 +1,81 @@
+export const SKILLS = {
+  "Web Dev": [
+    "HTML",
+    "CSS",
+    "JS",
+    "React",
+    "Vue",
+    "NextJS",
+    "Svelte",
+    "SvelteKit",
+    "Redux",
+    "Bitbucket",
+    "Fauna",
+    "Flask",
+    "Django",
+    "Gitpod",
+  ],
+  "Data Engineering": [
+    "Python",
+    "SQL",
+    "NoSQL",
+    "Big Data",
+    "Data Cleaning",
+    "Data Modelling",
+    "Data Scraping",
+    "Feature Engineering",
+    "Mmatplotlib",
+  ],
+  "AI Engineering": ["SKlearn", "Tensorflow", "OpenCV", "Vertex AI"],
+  "More skills": [
+    "Agile",
+    "Linux",
+    "Java",
+    "Cloudflare",
+    "JWT",
+    "Celery",
+    "Redis",
+    "VirtualBox",
+  ],
+};
+
+export const PROJECTS = [
+  {
+    name: "Urban Dhaba",
+    description:
+      "Project Description, some text here that can accurately describe the project...",
+    img: "img.jpg",
+    github: "https://github.com/manansoni77/Urban-Dhaba",
+  },
+  {
+    name: "CS50 Finance",
+    description:
+      "Project Description, some text here that can accurately describe the project...",
+    img: "img.jpg",
+    github: "https://github.com/manansoni77/CS50-Finance",
+    render: "https://cs50-finance-nx6i.onrender.com",
+  },
+  {
+    name: "Activity Tracker",
+    description:
+      "Project Description, some text here that can accurately describe the project...",
+    img: "img.jpg",
+    github: "https://github.com/manansoni77/ActivityTrackerFlask",
+    render: "https://activity-v1.onrender.com",
+  },
+  {
+    name: "Activity Tracker 2",
+    description:
+      "Project Description, some text here that can accurately describe the project...",
+    img: "img.jpg",
+    github: "https://github.com/manansoni77/ActivityTrackerFlaskV2",
+    render: "https://activity-v2-frontend.onrender.com",
+  },
+  {
+    name: "Object Recognition",
+    description:
+      "Project Description, some text here that can accurately describe the project...",
+    img: "img.jpg",
+    github: "https://github.com/manansoni77/Object-Recognition",
+  },
+];
